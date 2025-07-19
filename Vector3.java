572f237af0c06a0a0f2bdf0fedd891a88b85a711
Ivan-Busygin <ivan.busygin.work@gmail.com>
2025-07-19 23:41:20 +0700

    Исправил комментарии

--- Vector3.java
+++ Vector3.java
@@ -17,12 +17,12 @@ public class Vector3 {
         this.y += other.y;
         this.z += other.z;
     }
-    // Статический метод вычитания двух векторов
+    // Статический метод сложения двух векторов
     public static Vector3 add(Vector3 a, Vector3 b) {
         return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
     }
 
-    // Динамический метод сложения двух векторов
+    // Динамический метод вычитания двух векторов
     public void subtract(Vector3 other) {
         this.x -= other.x;
         this.y -= other.y;
