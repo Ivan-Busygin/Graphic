6afe1c70716550f72239cc249dbf63d0482e48b6
Ivan-Busygin <ivan.busygin.work@gmail.com>
2025-07-20 15:48:04 +0700

    Vector3

--- Vector3.java
+++ Vector3.java
@@ -1,62 +1,154 @@
-public class Vector3 {
+public class Vector3{
 
     double x;
     double y;  
     double z;
 
+    // Main constructor
     // Основной конструктор
-    public Vector3(double x, double y, double z) {
+    public Vector3(double x, double y, double z){
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
+    // Instance method for adding two vectors
     // Динамический метод сложения двух векторов
-    public void add(Vector3 other) {
+    public void add(Vector3 other){
         this.x += other.x;
         this.y += other.y;
         this.z += other.z;
     }
+    // Static method for adding two vectors
     // Статический метод сложения двух векторов
-    public static Vector3 add(Vector3 a, Vector3 b) {
+    public static Vector3 add(Vector3 a, Vector3 b){
         return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
     }
+    // Method for adding scalar to vector
+    // Метод сложения вектора и скаляра
+    public void addScalar(double scalar){
+        this.x += scalar;
+        this.y += scalar;
+        this.z += scalar;
+    }
 
+    // Instance method for subtracting two vectors
     // Динамический метод вычитания двух векторов
-    public void subtract(Vector3 other) {
+    public void subtract(Vector3 other){
         this.x -= other.x;
         this.y -= other.y;
         this.z -= other.z;
     }
+    // Static method for subtracting two vectors
     // Статический метод вычитания двух векторов
-    public static Vector3 subtract(Vector3 a, Vector3 b) {
+    public static Vector3 subtract(Vector3 a, Vector3 b){
         return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
     }
-}
+    // Method for subtracting scalar from vector
+    // Метод вычитания скаляра из вектора
+    public void subtractScalar(double scalar){
+        this.x -= scalar;
+        this.y -= scalar;
+        this.z -= scalar;
+    }
 
+    // Instance method for multiplying two vectors
+    // Динамический метод умножения двух векторов
+    public void multiply(Vector3 other){
+        this.x *= other.x;
+        this.y *= other.y;
+        this.z *= other.z;
+    }
+    // Static method for multiplying two vectors
+    // Динамический метод умножения двух векторов
+    public static Vector3 multiply(Vector3 a, Vector3 b){
+        return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
+    }
+    // Method for multiplying vector by scalar
+    // Метод умножения вектора на скаляр
+    public void multiplyScalar(double scalar){
+        this.x *= scalar;
+        this.y *= scalar;
+        this.z *= scalar;
+    }
 
-class Get_Distance {
-    public static double get_Distance(Vector3 point1, Vector3 point2) {
-        double delta_x = Math.abs(point1.x - point2.x);
-        double delta_y = Math.abs(point1.y - point2.y);
-        double delta_z = Math.abs(point1.z - point2.z);
-        // нашли изменение по осям координат
+    // Instance method for dividing two vectors
+    // Динамический метод деления двух векторов
+    public void divide(Vector3 other){
+        this.x /= other.x;
+        this.y /= other.y;
+        this.z /= other.z;
+    }
+    // Static method for dividing two vectors
+    // Динамический метод деления двух векторов
+    public static Vector3 divide(Vector3 a, Vector3 b){
+        return new Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
+    }
+    // Method for dividing vector by scalar
+    // Метод деления вектора на скаляр
+    public void divideScalar(double scalar) {
+        if (scalar != 0){
+            this.x /= scalar;
+            this.y /= scalar;
+            this.z /= scalar;
+        }
+    }
 
-        double delta_x_y = Math.sqrt(delta_x * delta_x + delta_y * delta_y);
-        // нашли изменение по xy
+    // Distance between two points
+    // Рассстояние между точками
+    public static double getDistance(Vector3 point1, Vector3 point2){
+        double deltaX = Math.abs(point1.x - point2.x);
+        double deltaY = Math.abs(point1.y - point2.y);
+        double deltaZ = Math.abs(point1.z - point2.z);
 
-        double distance = Math.sqrt(delta_x_y * delta_x_y + delta_z * delta_z);
-        // нашли дистанцию между 2 точками
+        double deltaXY = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        double distance = Math.sqrt(deltaXY * deltaXY + deltaZ * deltaZ);
+
         return distance;
     }
-}
 
+    // Negate a vector
+    // Отрицает вектор
+    public static Vector3 negative(Vector3 vector3){
+        return new Vector3(vector3.x * -1, vector3.y * -1, vector3.z * -1);
+    }
 
-class Test {
-    public static void main(String[] args) {
-        Vector3 a = new Vector3(0, 0, 0);
-        Vector3 b = new Vector3(1, 1, 1);
-        double distance = Get_Distance.get_Distance(a, b);
-        System.out.print(distance);
+    // Convert to string
+    // Преобрразование в строку
+    public String toString(){
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
+
+    // Length of vector
+    // Длинна вектора
+    public double length(Vector3 vector3){
+        return Math.sqrt(vector3.x * vector3.x + vector3.y * vector3.y + vector3.z * vector3.z);
+    }
+
+    // Normalize the vector
+    // Нормализация
+    public Vector3 normalize(){
+        double len = length(this);
+        if (len == 0){
+            return new Vector3(0, 0, 0);
+        }
+        else{
+            return new Vector3(x / len, y / len, z / len);
+        }
+    }
+    
+    // Dot product
+    // Скалярное произведение
+    public static double dot(Vector3 a, Vector3 b){
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    // Cross product
+    // Векторное произведение
+    public static Vector3 cross(Vector3 a, Vector3 b) {
+    return new Vector3(
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x);
     }
 }
